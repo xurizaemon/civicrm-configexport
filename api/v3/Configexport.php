e6cbed5f4af77ab31cbab268bc5d19da489ec84d
Chris Burgess <chris@fuzion.co.nz>
2015-09-25 12:19:40 +1000

    Add Configexport.export

--- api/v3/Configexport.php
+++ api/v3/Configexport.php
@@ -9,7 +9,8 @@
  * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
  */
 function _civicrm_api3_configexport_create_spec(&$spec) {
-  // $spec['some_parameter']['api.required'] = 1;
+  $spec['entity_type']['api.required'] = 1;
+  $spec['entity_id']['api.required'] = 1;
 }
 
 /**
@@ -43,4 +44,28 @@ function civicrm_api3_configexport_delete($params) {
  */
 function civicrm_api3_configexport_get($params) {
   return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+/**
+ * Configexport.create API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+function _civicrm_api3_configexport_export_spec(&$spec) {
+  $spec['entity_type']['api.required'] = 1;
+  $spec['entity_id']['api.required'] = 1;
+}
+
+/**
+ * Configexport.create API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_configexport_export($params) {
+  return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
 }
